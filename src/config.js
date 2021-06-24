404b84efbec95d035390c70ab37fda68dabb8f27
Gagan Malvi <malvigagan@gmail.com>
2021-06-24 21:03:47 +0530

    webinst: fixup
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- src/config.js
+++ src/config.js
@@ -3,65 +3,14 @@ export default {
     ACCENT_COLOR: "#0366d6",
     SUPPORTED_DEVICES: [
         {
-            name: "Pixel 2",
-            model: "walleye",
-        },
-        {
-            name: "Pixel 2 XL",
-            model: "taimen",
-        },
-        {
-            name: "Pixel 4a 5G",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 4",
-            model: "flame",
-        },
-        {
-            name: "Pixel 4 XL",
-            model: "coral",
+            name: "Teracube 2e",
+            model: "yk673v6_lwg62_64",
         },
     ],
     RELEASE_VARIANTS: {
-        minimal: {
-            description: "Minimal version without Google apps or services.",
-            suffix: "",
-        },
-        gapps: {
-            description:
-                "Version with Google services included, so the apps you’re familiar with will work. Recommended for most users.",
-            suffix: " with Google services",
-        },
-        test: {
-            description: "Experimental version for testing only.",
-            suffix: "experimental",
-        },
-        beta: {
-            description: "Beta version for testing only.",
-            suffix: "beta",
+        UNOFFICIAL: {
+            description: "Unofficial builds.",
+            suffix: "UNOFFICIAL",
         },
     },
-    DONATION_LINKS: [
-        {
-            title: "Recurring donation",
-            description:
-                "Please consider making a recurring donation on Patreon for benefits such as early access to updates, exclusive behind-the-scenes development news, and priority support. This allows us to support the project sustainably in the future.",
-            highlight: true,
-            url: "https://patreon.com/kdrag0n",
-            icon: "patreon",
-        },
-        {
-            title: "One-time donation",
-            description:
-                "You can also make a one-time donation through PayPal. It doesn’t help with sustainable support, but every little bit is appreciated!",
-            highlight: false,
-            url: "https://paypal.me/kdrag0ndonate",
-            icon: "paypal",
-        },
-    ],
 };
--- src/core/devices.js
+++ src/core/devices.js
@@ -1,17 +1,5 @@
 const DEVICE_NAMES = {
-    sailfish: "Pixel",
-    marlin: "Pixel XL",
-    walleye: "Pixel 2",
-    taimen: "Pixel 2 XL",
-    blueline: "Pixel 3",
-    crosshatch: "Pixel 3 XL",
-    sargo: "Pixel 3a",
-    bonito: "Pixel 3a XL",
-    flame: "Pixel 4",
-    coral: "Pixel 4 XL",
-    sunfish: "Pixel 4a",
-    bramble: "Pixel 4a 5G",
-    redfin: "Pixel 5",
+    yk673v6_lwg62_64: "Teracube 2e",
 };
 
 export function getDeviceName(device) {
